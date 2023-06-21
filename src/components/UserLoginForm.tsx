48193bf19e7682835e74b5a9f8b5cf712bd1b7f7
alapottra-troj1n <alapottratrojon@gmail.com>
2023-06-21 23:46:03 +0600

    added signin with google button

--- src/components/UserLoginForm.tsx
+++ src/components/UserLoginForm.tsx
@@ -1,12 +1,33 @@
-import { cn } from "@/lib/utils";
+'use client'
 
+import { cn } from "@/lib/utils";
+import { Button } from "./ui/Button";
+import { useState } from "react";
+import {signIn} from 'next-auth/react'
+import { BsGoogle } from 'react-icons/bs';
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {}
 
-export default function UserLoginForm({className}: Props) {
+export default function UserLoginForm({ className, ...props }: Props) {
+
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    const loginWithGoogle = async() => {
+        setIsLoading(true);
+        try {
+            await signIn('google')
+        } catch (error) {
+            //error notification
+
+            
+        }finally{
+            setIsLoading(false)
+        }
+    }
+
   return (
-    <div className={cn('flex justify-center', className)}>
-        <button>Google</button>
+    <div {...props} className={cn("flex justify-center", className)}>
+      <Button onClick={loginWithGoogle} isLoading={isLoading} size='sm' className="w-full" >{isLoading ? null : <BsGoogle className="text-lg mr-2"/>} Continue with Google </Button>
     </div>
-  )
+  );
 }
