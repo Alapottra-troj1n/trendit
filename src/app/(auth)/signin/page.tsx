79de7e56eb1ac7de915e66b37f2aaa9660ae88f0
alapottra-troj1n <alapottratrojon@gmail.com>
2023-06-24 01:14:06 +0600

    added intercept modal open and close functionality and changed the backtohome button style

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -2,19 +2,17 @@ import SignIn from "@/components/SignIn";
 import { buttonVariants } from "@/components/ui/Button";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-
+import { AiOutlineHome } from "react-icons/ai";
 export default function page() {
   return (
     <div className="absolute inset-0">
       <div className="h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20">
         <Link
           href={"/"}
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "self-start, -mt-20"
-          )}
+         className="flex items-center underline text-sm"
         >
-          Home
+          <AiOutlineHome className="text-white mr-2 text-lg" />
+          Back to Homepage
         </Link>
         <SignIn />
       </div>
--- src/app/@authModal/(.)signin/page.tsx
+++ src/app/@authModal/(.)signin/page.tsx
@@ -1,9 +1,22 @@
-type Props = {}
+'use client'
+import SignIn from "@/components/SignIn";
+import { CgClose } from "react-icons/cg";
+import { useRouter } from "next/navigation";
+
+type Props = {};
 
 export default function page({}: Props) {
+  const router = useRouter();
   return (
-    <div className=" pt-32">
-        <p className="text-slate-100">INTERCEPT</p>
+    <div className="fixed inset-0 bg-slate-950/50 text-white z-10">
+      <div className="container flex items-center h-full max-h-lg mx-auto">
+        <div className="relative bg-slate-800 lg:w-2/4 w-full mx-auto h-fit py-20 px-2 rounded-lg">
+          <div className="absolute top-4 right-4">
+            <CgClose onClick={()=> router.back()} className="cursor-pointer  text-white " />
+          </div>
+          <SignIn />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
